d61f46b807aa1d1b837c99218fe98b5e2ff3e129
G M Ganesh <gmganesh173@gmail.com>
2026-05-30 12:45:48 +0530

    Second largest number

---
+++ NamasteDsa/01_SecondLargest.js
@@ -0,0 +1,21 @@
+function secondLargest(arr) {
+
+    if (arr.length < 2) {
+        return null; // Not enough elements to find the second largest
+    }
+
+    let largest = -Infinity;
+    let second = -Infinity;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > largest) {
+            second = largest
+            largest = arr[i];
+        }
+        if (arr[i] < largest && arr[i] > second) {
+            second = arr[i]
+        }
+    }
+    return second;
+}
+console.log(secondLargest([1, 2, 3, 2, 44, 5, 5, 55, 4, 33, 2, 3]), "Second")
